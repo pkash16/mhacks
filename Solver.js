451c1b41aaf8660700951d9f0510d9d81c57f709
sohilpenguins <sohilpenguins@yahoo.com>
2016-02-20 16:32:23 -0500

    Working without grabbing

--- Solver.js
+++ Solver.js
@@ -16,7 +16,7 @@ var myFirebaseRef = new Firebase("https://torrid-fire-8164.firebaseio.com/Events
 
 
 function myJsFunction(){
-	iyear = document.getElementById('input1').value;
+	/*iyear = document.getElementById('input1').value;
 	imonth = document.getElementById('input2').value;
 	iday = document.getElementById('input3').value;
 	ihour = document.getElementById('input4').value;
@@ -30,11 +30,11 @@ function myJsFunction(){
 	timez.push(new Date(fyear,fmonth,fday,fhour,fminute,01,01));
 	document.write(timez[0]);
 	document.write(timez[1]);
+	*/
 
 
 
-
-       var randomseed = Math.floor((Math.random() * 1000) + 1);
+      /* var randomseed = Math.floor((Math.random() * 1000) + 1);
         var usersRef = myFirebaseRef.child(randomseed);
 
             var hopperRef = usersRef.child(name);
@@ -46,12 +46,23 @@ function myJsFunction(){
 
 	//FOR CREATTED DB
 
-//         var superref = hopperRef;
-  //          superref.update({
-	//			"name" : name,
-	//			"itime": timez[0].toDateString(),
-	//			"ftime": timez[1].toDateString()
-      //  });
+         var superref = hopperRef;
+            superref.update({
+				"name" : name,
+				"itime": timez[0].toDateString(),
+				"ftime": timez[1].toDateString()
+        });
 
+ */
+
+	// Get a database reference to our posts
+	var ref = new Firebase("https://torrid-fire-8164.firebaseio.com/Events/116");
+
+// Attach an asynchronous callback to read the data at our posts reference
+	ref.on("value", function(snapshot) {
+		document.write(snapshot.valueOf().toString());
+	}, function (errorObject) {
+		document.write("The read failed: " + errorObject.code);
+	});
  }
 
